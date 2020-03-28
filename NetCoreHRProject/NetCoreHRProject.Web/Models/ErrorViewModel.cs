98eebedb7695f76b244afbdac64ebe9dfd939a0d
Hamzah Mahafzah <netcorehr2020@gmail.com>
2020-03-28 22:18:02 +0300

    Create Solution Structure Cont

---
+++ NetCoreHRProject/NetCoreHRProject.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCoreHRProject.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
